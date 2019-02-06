85e7ce38e7b3c5a54aa62d5ed0b56acdd6edf889
Prabin Paudel <prabinpaudel@Prabins-MacBook-Pro.local>
2019-02-05 23:41:08 -0500

    Formatting of the code

--- RemoveDuplicatesFromSortedList.java
+++ RemoveDuplicatesFromSortedList.java
@@ -1,43 +1,46 @@
 /**
- * Solution to https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+ * Solution to
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
  * 
- * Output from LeetCode: 
+ * Output from LeetCode:
  * 
- * Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Duplicates from Sorted List II.
- * Memory Usage: 27.2 MB, less than 2.30% of Java online submissions for Remove Duplicates from Sorted List II.
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove
+ * Duplicates from Sorted List II. Memory Usage: 27.2 MB, less than 2.30% of
+ * Java online submissions for Remove Duplicates from Sorted List II.
  */
 public class RemoveDuplicatesFromSortedList {
-	
-	static class ListNode{
+
+	static class ListNode {
 		int val;
 		ListNode next = null;
-		ListNode(int val){
+
+		ListNode(int val) {
 			this.val = val;
 		}
 	}
 
 	public RemoveDuplicatesFromSortedList() {
 	}
-	
+
 	static ListNode deleteDuplicates(ListNode head) {
-        ListNode current = head;
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode previous = dummy;
-        
-        while(current != null && current.next != null){
-            if (current.val == current.next.val){
-                while (current.next != null && current.val == current.next.val){
-                    current = current.next;
-                }
-                previous.next = current.next;
-            }else{
-                previous = current;
-            }
-            current = current.next;
-        }
-        return dummy.next;
-    }
+		ListNode current = head;
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		ListNode previous = dummy;
+
+		while (current != null && current.next != null) {
+			if (current.val == current.next.val) {
+				while (current.next != null && current.val == current.next.val) {
+					current = current.next;
+				}
+				previous.next = current.next;
+			} else {
+				previous = current;
+			}
+			current = current.next;
+		}
+		return dummy.next;
+	}
 
 	public static void main(String[] args) {
 		// Input : 1-> 1-> 2-> 2-> 4-> 5-> 6-> 7-> 7
@@ -60,8 +63,8 @@ public class RemoveDuplicatesFromSortedList {
 		ListNode seven_2 = new ListNode(7);
 		seven.next = seven_2;
 		ListNode res = RemoveDuplicatesFromSortedList.deleteDuplicates(head);
-		//Print Result
-		while(res != null) {
+		// Print Result
+		while (res != null) {
 			System.out.print(res.val + " -> ");
 			res = res.next;
 		}
